12efae819bad124df551f613c86af040448e7ae9
taemath <hirotaka081630@gmail.com>
2022-03-01 08:09:40 +0900

    refactor(assembly_line): クラスの定数、privateメソッドの分割

--- assembly-line/assembly_line.rb
+++ assembly-line/assembly_line.rb
@@ -1,26 +1,29 @@
 class AssemblyLine
+  HOURLY_YIELD = 221
+
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    num = 221
-    case @speed
-    when 1..4
-      @speed * num
-    when 5..8
-      @speed * num * 0.9
-    when 9
-      @speed * num * 0.8
-    when 10
-      @speed * num * 0.77
-    end
+    @speed * HOURLY_YIELD * success_rate
   end
 
   def working_items_per_minute
     (production_rate_per_hour / 60).floor
   end
-end
 
-p AssemblyLine.new(6).production_rate_per_hour
-p AssemblyLine.new(6).working_items_per_minute
+  private
+  def success_rate
+    case @speed
+    when 1..4
+      1
+    when 5..8
+      0.9
+    when 9
+      0.8
+    when 10
+      0.77
+    end
+  end
+end
