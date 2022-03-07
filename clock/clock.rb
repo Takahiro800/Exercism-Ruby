5a93778d833965bdb7030b4a74d9333cf7c69c35
taemath <hirotaka081630@gmail.com>
2022-03-07 21:49:37 +0900

    feat(clock): submit but failer

---
+++ clock/clock.rb
@@ -0,0 +1,33 @@
+class Clock
+  MINUTES_PER_HOUR = 60
+  HOURS_PER_DAY = 24
+  MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY
+
+  attr_reader :total_minutes
+
+  def initialize(hour: 00 , minute: 00)
+    @total_minutes = (hour * MINUTES_PER_HOUR + minute) % MINUTES_PER_DAY
+  end
+
+  def hour
+    (total_minutes / MINUTES_PER_HOUR) % HOURS_PER_DAY
+  end
+
+  def minute
+    total_minutes % MINUTES_PER_HOUR
+  end
+
+  def to_s
+    "%02d:%02d" %[hour, minute]
+  end
+
+  def +(other)
+    @total_minutes += other.total_minutes
+    to_s
+  end
+
+  def -(other)
+    @total_minutes -= other.total_minutes
+    to_s
+  end
+end
