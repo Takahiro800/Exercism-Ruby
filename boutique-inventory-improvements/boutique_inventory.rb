74306e18593286839400de9883670d4871cf8c82
taemath <hirotaka081630@gmail.com>
2022-04-22 20:03:40 +0900

    refactor(boutique_inventory): リファクタリング

--- boutique-inventory-improvements/boutique_inventory.rb
+++ boutique-inventory-improvements/boutique_inventory.rb
@@ -4,16 +4,14 @@ class BoutiqueInventory
   attr_reader :items
 
   def initialize(items)
-    @items = items.map{ |item| OpenStruct.new(price: item[:price], name: item[:name], quantity_by_size: item[:quantity_by_size]) }
+    @items = items.map{ |item| OpenStruct.new(item) }
   end
 
   def item_names
-    items.map { |item| item.name }.sort
+    items.map(&:name).sort
   end
 
   def total_stock
-    items.sum do |item|
-      item.quantity_by_size.values.sum
-    end
+    items.map(&:quantity_by_size).map(&:values).sum(&:sum)
   end
 end
