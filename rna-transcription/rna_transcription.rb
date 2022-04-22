9086207e046e4132026afda5a1a87cb7c2664e94
taemath <hirotaka081630@gmail.com>
2022-04-22 20:40:51 +0900

    refactor(rna_transciption): string#trメソッド

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,15 +1,23 @@
 class Complement
-  def self.of_dna(dna)
-    dna.split("").map{ |dna| return_nucleotide(dna) }.join
+
+  DNA = 'CATG'
+  RNA = 'GUAC'
+
+  def self.of_dna(nucleotides)
+    nucleotides.tr(DNA, RNA)
   end
 
-  def self.return_nucleotide(dna)
-    return "" if dna.nil?
-    case dna
-      when 'G' then  return 'C'
-      when 'C' then  return 'G'
-      when 'T' then  return 'A'
-      when 'A' then  return 'U'
-    end
-  end
+  # def self.of_dna(dna)
+  #   dna.split("").map{ |dna| return_nucleotide(dna) }.join
+  # end
+
+  # def self.return_nucleotide(dna)
+  #   return "" if dna.nil?
+  #   case dna
+  #     when 'G' then  return 'C'
+  #     when 'C' then  return 'G'
+  #     when 'T' then  return 'A'
+  #     when 'A' then  return 'U'
+  #   end
+  # end
 end
