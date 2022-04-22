61a1479352c4d9d029031550c842dc39e2e39306
taemath <hirotaka081630@gmail.com>
2022-04-23 08:01:51 +0900

    refactor: leap.rb

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,16 +1,5 @@
-=begin
-Write your code for the 'Leap' exercise in this file. Make the tests in
-`leap_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/leap` directory.
-=end
-
 class Year
   def self.leap?(year)
-    return true if year % 400 == 0
-    return false if year % 100 == 0
-    return true if year % 4 == 0
-    return false
+    year % 4 == 0 and year % 100 != 0 or year % 400 == 0
   end
 end
