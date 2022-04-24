0ee9181fc68fe1f9fbc80ca16390a65d56b591e5
taemath <hirotaka081630@gmail.com>
2022-04-24 22:04:49 +0900

    fix(tournament): +,-メソッドの返り値クラスインスタンスに修正

--- tournament/tournament.rb
+++ tournament/tournament.rb
@@ -14,31 +14,33 @@ class Tournament
 
   def <=>(other)
     if other.is_a?(Tournament)
-        return p <=> other.p  if p != other.p
-        return 1 if name < other.name
-        return -1 if name > other.name
+      return p <=> other.p if p != other.p
+      return 1 if name < other.name
+      return -1 if name > other.name
     end
   end
 
   def self.tally(input)
     teams = []
 
-    input.split("\n").each do |line|
-      puts line
-      words = line.split(';')
+    input
+      .split("\n")
+      .each do |line|
+        puts line
+        words = line.split(';')
 
-      case words[2]
-      when "win"
-        find_or_create_team(teams, words[0], "win")
-        find_or_create_team(teams, words[1], "loss")
-      when "draw"
-        find_or_create_team(teams, words[0], "draw")
-        find_or_create_team(teams, words[1], "draw")
-      when "loss"
-        find_or_create_team(teams, words[0], "loss")
-        find_or_create_team(teams, words[1], "win")
+        case words[2]
+        when 'win'
+          find_or_create_team(teams, words[0], 'win')
+          find_or_create_team(teams, words[1], 'loss')
+        when 'draw'
+          find_or_create_team(teams, words[0], 'draw')
+          find_or_create_team(teams, words[1], 'draw')
+        when 'loss'
+          find_or_create_team(teams, words[0], 'loss')
+          find_or_create_team(teams, words[1], 'win')
+        end
       end
-    end
 
     expected = <<~TALLY
       Team                           | MP |  W |  D |  L |  P
@@ -47,17 +49,15 @@ class Tournament
     teams.sort!.reverse!
 
     teams.each do |team|
-      team.name += "                                "
-      expected << "#{team.name[0,30]} |  #{team.mp} |  #{team.w} |  #{team.d} |  #{team.l} |  #{team.p}\n"
+      team.name += '                                '
+      expected << "#{team.name[0, 30]} |  #{team.mp} |  #{team.w} |  #{team.d} |  #{team.l} |  #{team.p}\n"
     end
 
     return expected
-
   end
 
-
   def self.find_or_create_team(teams, name, result)
-    team = teams.find {|team| team.name == name}
+    team = teams.find { |team| team.name == name }
     if team
       update_point(team, result)
     else
@@ -70,13 +70,13 @@ class Tournament
   def self.update_point(team, result)
     team.mp += 1
     case result
-    when "win"
+    when 'win'
       team.w += 1
       team.p += 3
-    when "draw"
+    when 'draw'
       team.p += 1
       team.d += 1
-    when "loss"
+    when 'loss'
       team.l += 1
     end
   end
