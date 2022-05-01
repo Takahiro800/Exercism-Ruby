83e3a4be973ef810c56314a0da9b53f24bdef2b2
taemath <hirotaka081630@gmail.com>
2022-05-01 19:58:22 +0900

    refactor(phone number): 正規表現、一致しない場合にnilを返すようにStringクラスの抽出機能を活用した

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,25 +1,32 @@
 class PhoneNumber
+  VALID_PHONE_NUMBER = /^([2-9]\d\d){2}\d{4}$/
+
   def self.clean(number)
-    num = number.gsub('-', ' ').gsub('.', ' ')
-    num =
-      num.split.join.delete(
-        '(' \
-          ')',
-      )
-
-    num = self.delete_country_code(num)
-    return nil if num.length != 10 || !((2..9).include?(num[0].to_i) && (2..9).include?(num[3].to_i))
-    return num
+    numbers = number.gsub(/\D/, '').sub(/^1/, '')
+    numbers[VALID_PHONE_NUMBER]
   end
 
-  private
+  # def self.clean(number)
+  #   num = number.gsub('-', ' ').gsub('.', ' ')
+  #   num =
+  #     num.split.join.delete(
+  #       '(' \
+  #         ')',
+  #     )
 
-  def self.delete_country_code(num)
-    if num.length == 11 && num[0] == '1'
-      num = num[1..]
-    elsif num.length == 12 && num[0..1] == '+1'
-      num = num[2..]
-    end
-    return num
-  end
+  #   num = self.delete_country_code(num)
+  #   return nil if num.length != 10 || !((2..9).include?(num[0].to_i) && (2..9).include?(num[3].to_i))
+  #   return num
+  # end
+
+  # private
+
+  # def self.delete_country_code(num)
+  #   if num.length == 11 && num[0] == '1'
+  #     num = num[1..]
+  #   elsif num.length == 12 && num[0..1] == '+1'
+  #     num = num[2..]
+  #   end
+  #   return num
+  # end
 end
