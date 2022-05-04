d3110527fe155508bded549177ed95f5d8d62de4
taemath <hirotaka081630@gmail.com>
2022-05-04 11:38:34 +0900

    refactor(sieve): 計算量がN^2になってる

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -4,12 +4,6 @@ class Sieve
   end
 
   def primes
-    prime = []
-    (1..@num).to_a.each do |num|
-      divisor = 0
-      (1..num).each { |i| divisor += 1 if num % i == 0 }
-      prime << num if divisor == 2
-    end
-    return prime
+    (1..@num).to_a.select { |n| (1..n).count { |i| n % i == 0 } == 2 }
   end
 end
