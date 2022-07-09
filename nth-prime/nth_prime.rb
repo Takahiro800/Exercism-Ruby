bae9b8940188c22319765c9bbeea2f6468d65913
taemath <hirotaka081630@gmail.com>
2022-07-09 10:25:38 +0900

    refactor(nth_prime): エラトステネスの篩・next

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,16 +1,24 @@
 class Prime
-  PRIME_NUMBERS = [2]
+  # PRIME_NUMBERS = [2]
+  MAX_NUMBER = 10**6
 
   def self.nth(n)
     raise ArgumentError if n < 1
 
-    tmp = PRIME_NUMBERS.max + 1
+    is_prime = Array.new(MAX_NUMBER, true)
+    is_prime[0] = false
+    is_prime[1] = false
 
-    while PRIME_NUMBERS.length < n
-      PRIME_NUMBERS << tmp if PRIME_NUMBERS.none? { |prime| tmp % prime == 0 }
-      tmp += 1
+    count = 0
+    [*2..MAX_NUMBER].each do |i|
+      next unless is_prime[i]
+
+      count += 1
+      return i if count == n
+
+      ((2 * i)..MAX_NUMBER).step(i).each { |j| is_prime[j] = false }
     end
 
-    return PRIME_NUMBERS[n - 1]
+    prime[n]
   end
 end
