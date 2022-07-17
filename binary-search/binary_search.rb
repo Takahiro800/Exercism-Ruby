40b94a0c0d5904e4fcdcec34d72b6028234be6d5
taemath <hirotaka081630@gmail.com>
2022-07-17 14:48:50 +0900

    refactor(binary-search): 再帰を使った

--- binary-search/binary_search.rb
+++ binary-search/binary_search.rb
@@ -1,66 +1,25 @@
 class BinarySearch
-  attr_reader :root
+  attr_reader :array
 
   def initialize(nums_array)
-    return null if nums_array.size == 0
-
-    @root = sortedArrayToBSTHelper(nums_array, 0, nums_array.size - 1)
+    @array = nums_array
   end
 
   def search_for(number)
-    iterator = @root
-    depth = 0
-    while iterator != 'None'
-      if iterator.data == number
-        p 'match!!!'
-        iterator.data
-      end
-
-      if iterator.data > number
-        p "number: #{number}, iterator.data: #{iterator.data}, result: left"
-        iterator = iterator.left
-      else
-        p "number: #{number}, iterator.data: #{iterator.data}, result: right"
-        iterator = iterator.right
-      end
-    end
-
-    return iterator != 'None' ? iterator.data : 'None'
+    return nil if array.size == 0
+    search_for_helper(number, 0, array.size - 1)
   end
 
   private
 
-  def sortedArrayToBSTHelper(nums_array, start, last)
-    return BinaryTree.new(nums_array[start]) if start == last
+  def search_for_helper(key, start, last)
+    return nil if start > last
 
     mid = ((start + last) / 2).floor
-    if (mid - 1 >= start)
-      left = sortedArrayToBSTHelper(nums_array, start, mid - 1)
-    else
-      left = 'None'
-    end
+    return mid if array[mid] == key
 
-    if (mid + 1 <= last)
-      right = sortedArrayToBSTHelper(nums_array, mid + 1, last)
-    else
-      right = 'None'
-    end
+    return nil if start == last
 
-    @root = BinaryTree.new(nums_array[mid], left, right)
+    key < array[mid] ? search_for_helper(key, start, mid - 1) : search_for_helper(key, mid + 1, last)
   end
 end
-
-class BinaryTree
-  attr_accessor :data, :right, :left
-
-  def initialize(data, left = 'None', right = 'None')
-    @data = data
-    @left = left
-    @right = right
-  end
-end
-
-binary = BinarySearch.new([1, 3, 4, 6, 8, 9, 11])
-
-p binary
-p binary.search_for(4)
