907fa76bf8b6ac559fd142c6328d56aa2a27a495
taemath <hirotaka081630@gmail.com>
2022-08-02 21:37:59 +0900

    refactor(protein-translation): .to_symでstringをシンボルに変換できる

--- protein-translation/protein_translation.rb
+++ protein-translation/protein_translation.rb
@@ -3,34 +3,34 @@ end
 
 class Translation
   CODON_TO_PROTEIN = {
-    'AUG' => 'Methionine',
-    'UUU' => 'Phenylalanine',
-    'UUC' => 'Phenylalanine',
-    'UUA' => 'Leucine',
-    'UUG' => 'Leucine',
-    'UCU' => 'Serine',
-    'UCC' => 'Serine',
-    'UCA' => 'Serine',
-    'UCG' => 'Serine',
-    'UAU' => 'Tyrosine',
-    'UAC' => 'Tyrosine',
-    'UGU' => 'Cysteine',
-    'UGC' => 'Cysteine',
-    'UGG' => 'Tryptophan',
-    'UAA' => 'STOP',
-    'UAG' => 'STOP',
-    'UGA' => 'STOP',
+    AUG: 'Methionine',
+    UUU: 'Phenylalanine',
+    UUC: 'Phenylalanine',
+    UUA: 'Leucine',
+    UUG: 'Leucine',
+    UCU: 'Serine',
+    UCC: 'Serine',
+    UCA: 'Serine',
+    UCG: 'Serine',
+    UAU: 'Tyrosine',
+    UAC: 'Tyrosine',
+    UGU: 'Cysteine',
+    UGC: 'Cysteine',
+    UGG: 'Tryptophan',
+    UAA: 'STOP',
+    UAG: 'STOP',
+    UGA: 'STOP',
   }
 
   def self.of_codon(codon)
-    raise false unless CODON_TO_PROTEIN.key?(codon)
+    raise false unless CODON_TO_PROTEIN.key?(codon.to_sym)
 
-    CODON_TO_PROTEIN[codon]
+    CODON_TO_PROTEIN[codon.to_sym]
   end
 
   def self.of_rna(strand)
     codons = strand.scan(/.{1,#{3}}/)
-    raise InvalidCodonError unless codons.reject { |codon| CODON_TO_PROTEIN.key?(codon) }.empty?
+    raise InvalidCodonError unless codons.reject { |codon| CODON_TO_PROTEIN.key?(codon.to_sym) }.empty?
     if codons.include?('UAA')
       i = codons.index('UAA')
       codons = codons[...i]
@@ -38,7 +38,3 @@ class Translation
     codons.map { |codon| self.of_codon(codon) }
   end
 end
-
-strand = 'CARROT'
-strand = 'UGGUGUUAUUAAUGGUUU'
-p Translation.of_rna(strand)
