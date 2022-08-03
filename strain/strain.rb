9e5067454954d7184b613fe89c6badfb3893f43c
taemath <hirotaka081630@gmail.com>
2022-08-03 21:11:17 +0900

    feat(strain): yield, block

--- strain/strain.rb
+++ strain/strain.rb
@@ -1,6 +1,11 @@
 class Array
   def keep
-    return yield
+    kept = []
+    each { |item| kept << item if yield item }
+    kept
   end
 
+  def discard
+    keep { |item| not yield item }
+  end
 end
